76691c3f26d53f9a41a9352ba1b135119d1b0001
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2017-10-23 10:47:05 -0500

    Fixed timing of done function in queries

--- server/routes/todo_router.js
+++ server/routes/todo_router.js
@@ -19,13 +19,14 @@ var pool = require('../modules/pool.js');
 router.get('/',function(req,res){
   console.log('in the GET route');
   pool.connect(function(errorConnectingToDb, db, done){
-    done();
+    
     if(errorConnectingToDb) {
       console.log('Error connecting to DB');
       res.sendStatus(500);
     } else {
       var queryText = 'SELECT * FROM "todo_list" ORDER BY "todo_complete", "todo_duedate", "todo_id"';
       db.query(queryText, function(errorQueryingDb, result){
+        done();
         if (errorQueryingDb){
           console.log('Error querrying database in GET route');
           console.log(queryText);
@@ -45,13 +46,13 @@ router.post('/', function(req,res){
   var newTodo = req.body;
   console.log(newTodo);
   pool.connect(function(errorConnectingToDb,db,done){
-    done();
     if(errorConnectingToDb){
       console.log('Error connecting to DB');
       res.sendStatus(500);
     } else {
       var queryText = 'INSERT INTO "todo_list" ("todo_text","todo_duedate") VALUES ($1, $2)';
       db.query(queryText,[newTodo.todo_text,newTodo.todo_duedate],function(errorQueryingDb,result) {
+        done();
         if (errorQueryingDb) {
           console.log('Error in POST route querying database with');
           console.log(queryText);
@@ -70,13 +71,13 @@ router.delete('/:id',function(req,res){
   var todoId = req.params.id;
   console.log('Deleting to-do',todoId);
   pool.connect(function(errorConnectingToDb,db,done){
-    done();
     if (errorConnectingToDb){
       console.log('Error connecting to database in DELETE route');
       res.sendStatus(500);
     } else {
       var queryText = 'DELETE FROM "todo_list" WHERE "todo_id" = $1;';
       db.query(queryText,[todoId],function(errorQueryingDb){
+        done();
         if (errorQueryingDb){
           console.log('Error in DELETE query');
           console.log(queryText);
@@ -94,13 +95,13 @@ router.put('/:id',function(req,res){
   var todoId = req.params.id;
   console.log('Toggling complete on to-do',todoId);
   pool.connect(function(errorConnectingToDb,db,done){
-    done();
     if (errorConnectingToDb){
       console.log('Error connecting to database in PUT route');
       res.sendStatus(500);
     } else {
       var queryText = 'UPDATE "todo_list" SET "todo_complete" = NOT "todo_complete" WHERE "todo_id" = $1;';
       db.query(queryText,[todoId],function(errorQueryingDb){
+        done();
         if (errorQueryingDb){
           console.log('Error in UPDATE query');
           res.sendStatus(500);
